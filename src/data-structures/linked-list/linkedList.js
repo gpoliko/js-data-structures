a29fd565f0b840a0443116204b7c2201fc7999d5
gio-poliko <gpoliko@gmail.com>
2020-12-22 19:30:00 +1300

    tidy up tests for linkedList. Rename linkedList functions according to Java Docs

--- src/data-structures/linked-list/linkedList.js
+++ src/data-structures/linked-list/linkedList.js
@@ -11,14 +11,14 @@ class LinkedList {
 
   toString (callback) {
     // console.log("THIS =", this)
-    return this.convertToArray(node => node.toString(callback)).toString()
+    return this.toArray(node => node.toString(callback)).toString()
   }
 
   /**
-     * convertToArray - creates an array of nodes
+     * toArray - creates an array of nodes
      * @return array of nodes
      */
-  convertToArray () {
+  toArray () {
     const nodes = []
     let currentNode = this.head
 
@@ -30,31 +30,13 @@ class LinkedList {
   }
 
   /**
-     * Adds values to the front of the list
-     * @param {Object} value
-     * @return entire LinkedList
-     */
-  prepend (value) {
-    // Create a new node to become a head
-    const newNode = new LinkedListNode(value, this.head)
-    this.head = newNode
-
-    // If no tail exists, newNode will become the tail
-    if (!this.tail) {
-      this.tail = newNode
-    }
-
-    return this
-  }
-
-  /**
      * Adds values to the end of the list
-     * @param {Object} value
+     * @param {Object} data
      * @return entire LinkedList
      */
-  append (value) {
+  add (data) {
     // Create a new node
-    const newNode = new LinkedListNode(value)
+    const newNode = new LinkedListNode(data)
 
     // If no head exists, newNode will become the head
     if (!this.head) {
@@ -70,6 +52,25 @@ class LinkedList {
 
     return this
   }
+  
+  /**
+   * Adds values to the front of the list
+   * @param {Object} data
+   * @return entire LinkedList
+   */
+  addFirst (data) {
+      // Create a new node to become a head
+      const newNode = new LinkedListNode(data, this.head)
+      this.head = newNode
+      
+      // If no tail exists, newNode will become the tail
+      if (!this.tail) {
+          this.tail = newNode
+        }
+        
+        return this
+  }
 }
+
 
 export default LinkedList
--- src/data-structures/linked-list/tests/linkedList.test.js
+++ src/data-structures/linked-list/tests/linkedList.test.js
@@ -8,12 +8,27 @@ describe('Linked List Tests', () => {
     expect(newList.toString()).toBe('')
   })
 
-  it('should add a new node to the linked list', () => {
+  it('should add a new node to the end of the linked list', () => {
     const newList = new LinkedList()
 
-    newList.append('testing-123')
+    newList.add('testing-123')
     expect(newList.toString()).toBe('testing-123')
     expect(newList.head).toEqual({data: 'testing-123', next: null})
     expect(newList.tail.next).toBeNull()
+  })
+
+  it('should add a node to the beginning of the linked list', () => {
+    const newList = new LinkedList()
+    
+    newList.addFirst('test1')
+    expect(newList.head).toEqual({data: 'test1', next: null})
+    expect(newList.tail).toEqual({data: 'test1', next: null})
+    expect(newList.head.toString()).toBe('test1')
+    expect(newList.tail.toString()).toBe('test1')
+
+    newList.add('test2')
+    newList.addFirst('test3')
+    
+
   })
 })
