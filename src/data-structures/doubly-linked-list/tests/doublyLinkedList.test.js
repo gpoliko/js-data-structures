845c341c60387224f461ff5c31c28baa6f287c7b
gio-poliko <gpoliko@gmail.com>
2021-01-02 14:35:36 +1300

    remove Node declaration as not being used yet - failing linting

--- src/data-structures/doubly-linked-list/tests/doublyLinkedList.test.js
+++ src/data-structures/doubly-linked-list/tests/doublyLinkedList.test.js
@@ -1,11 +1,10 @@
 import DoublyLinkedList from '../doublyLinkedList'
-import Node from '../node'
 
 describe('Doubly Linked List Tests', () => {
-    it('should create a new doubly linked list', () => {
-        const newList = new DoublyLinkedList()
+  it('should create a new doubly linked list', () => {
+    const newList = new DoublyLinkedList()
 
-        expect(newList).toEqual({ head: null, tail: null})
-        expect(newList.toString()).toBe('')
-    })
+    expect(newList).toEqual({ head: null, tail: null })
+    expect(newList.toString()).toBe('')
+  })
 })
