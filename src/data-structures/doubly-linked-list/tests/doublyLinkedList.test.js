4074fe14081f7ed82b17165f79b4d174ca193fef
gio-poliko <gpoliko@gmail.com>
2021-01-04 21:45:57 +1300

    correct addFirst function to create a new node instead of new doublyLinkedList. Added comments and tests.

--- src/data-structures/doubly-linked-list/tests/doublyLinkedList.test.js
+++ src/data-structures/doubly-linked-list/tests/doublyLinkedList.test.js
@@ -20,4 +20,20 @@ describe('Doubly Linked List Tests', () => {
     expect(newList.tail.previous.data).toBe(1)
     expect(newList.tail.next).toBeNull()
   })
+
+  it('should add node to the beginning of the linked list', () => {
+    const newList = new DoublyLinkedList()
+
+    newList.addFirst(5)
+    expect(newList.head.toString()).toBe('5')
+    expect(newList.tail.toString()).toBe('5')
+
+    newList.add(2)
+    newList.addFirst(3)
+
+    expect(newList.head.next.next.previous).toBe(newList.head.next)
+    expect(newList.tail.previous.next).toBe(newList.tail)
+    expect(newList.tail.previous.data).toBe(5)
+    expect(newList.toString()).toBe('3,5,2')
+  })
 })
