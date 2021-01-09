56596804dd4c35ae02769d65255d3c7e8e4a8fa7
gio-poliko <gpoliko@gmail.com>
2021-01-09 21:38:54 +1300

    tidy up tests for linkedlist node

--- src/data-structures/linked-list/tests/node.test.js
+++ src/data-structures/linked-list/tests/node.test.js
@@ -2,42 +2,33 @@ import Node from '../node'
 
 describe('Linked List Node', () => {
   it('should create a node containing data', () => {
-    const n1 = new Node(9)
+    const n1 = new Node(3)
     const n2 = new Node('test')
 
-    expect(n1.data).toBe(9)
+    expect(n1.data).toBe(3)
     expect(n1.next).toBeNull()
     expect(n2.data).toBe('test')
     expect(n2.next).toBeNull()
   })
 
   it('should link a node to another node', () => {
-    const n4 = new Node(4)
+    const n4 = new Node(2)
     const n3 = new Node('three', n4)
 
-    expect(n4.data).toBe(4)
+    expect(n4.data).toBe(2)
     expect(n3.data).toBe('three')
     expect(n3.next).toBe(n4)
     expect(n4.next).toBeNull()
   })
 
-  it("should allow a node's data to be an object", () => {
-    const n2Data = { d1: 'test1', d2: 'test2' }
-    const n2 = new Node(n2Data)
-
-    expect(n2.data.d1).toBe('test1')
-    expect(n2.data.d2).toBe('test2')
-  })
-
-  // Used example
   it('should convert a node to a string', () => {
     const n1 = new Node(5)
 
     expect(n1.toString()).toBe('5')
 
-    n1.data = 'test'
+    n1.data = 'test-123'
 
-    expect(n1.toString()).toBe('test')
+    expect(n1.toString()).toBe('test-123')
   })
 
   it('should convert a node to string with our defined stringifier', () => {
@@ -48,26 +39,5 @@ describe('Linked List Node', () => {
 
     expect(typeof callBackResult).toBe('string')
     expect(callBackResult).toBe('d1: 1, d2: 2')
-  })
-
-  it('should return unchange node data format with our defined stringifier', () => {
-    const objData = { d1: 1, d2: 2 }
-    const strData = '5'
-    const intData = 5
-
-    const objNode = new Node(objData)
-    const strNode = new Node(strData)
-    const intNode = new Node(intData)
-
-    const test1 = objNode.toString()
-    const test2 = strNode.toString()
-    const test3 = intNode.toString()
-
-    expect(typeof test1).toBe('string')
-    expect(typeof test2).toBe('string')
-    expect(typeof test3).toBe('string')
-    expect(test1).toBe('[object Object]')
-    expect(test2).toBe('5')
-    expect(test3).toBe('5')
   })
 })
