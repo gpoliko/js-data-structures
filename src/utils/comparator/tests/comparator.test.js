d82a21fdb3b02d4c2ded461eac131558bf6b1bdc
gio-poliko <gpoliko@gmail.com>
2021-01-17 16:41:08 +1300

    fix linting

--- src/utils/comparator/tests/comparator.test.js
+++ src/utils/comparator/tests/comparator.test.js
@@ -11,25 +11,25 @@ describe('Comparator Tests', () => {
     expect(compare.equal('test123', 'test321')).toBe(false)
   })
 
-  it('should use the class\'s compare function for less than comparison', () => {
-      const compare = new Comparator()
+  it('should use the class\'s compare function for lessThan comparison', () => {
+    const compare = new Comparator()
 
-      expect(compare.lessThan(1, 1)).toBe(false)
-      expect(compare.lessThan(1, 2)).toBe(true)
-      expect(compare.lessThan(2, 1)).toBe(false)
-      expect(compare.lessThan('test', 'test')).toBe(false)
-      expect(compare.lessThan('test', 'test123')).toBe(true)
-      expect(compare.lessThan('test123', 'test')).toBe(false)
+    expect(compare.lessThan(1, 1)).toBe(false)
+    expect(compare.lessThan(1, 2)).toBe(true)
+    expect(compare.lessThan(2, 1)).toBe(false)
+    expect(compare.lessThan('test', 'test')).toBe(false)
+    expect(compare.lessThan('test', 'test123')).toBe(true)
+    expect(compare.lessThan('test123', 'test')).toBe(false)
   })
 
-  it('should use the class\'s compare function for greater than comparison', () => {
-      const compare = new Comparator()
+  it('should use the class\'s compare function for greaterThan comparison', () => {
+    const compare = new Comparator()
 
-      expect(compare.greaterThan(1, 1)).toBe(false)
-      expect(compare.greaterThan(1, 2)).toBe(false)
-      expect(compare.greaterThan(2, 1)).toBe(true)
-      expect(compare.greaterThan('test', 'test')).toBe(false)
-      expect(compare.greaterThan('test', 'test123')).toBe(false)
-      expect(compare.greaterThan('test123', 'test')).toBe(true)
+    expect(compare.greaterThan(1, 1)).toBe(false)
+    expect(compare.greaterThan(1, 2)).toBe(false)
+    expect(compare.greaterThan(2, 1)).toBe(true)
+    expect(compare.greaterThan('test', 'test')).toBe(false)
+    expect(compare.greaterThan('test', 'test123')).toBe(false)
+    expect(compare.greaterThan('test123', 'test')).toBe(true)
   })
 })
